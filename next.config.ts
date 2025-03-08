ce14e9065d33ac6fa833804523fdb812c46d8d13
vishalBelwal <vishalbelwal52@gmail.com>
2025-03-09 00:28:28 +0530

    .

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/smartflow/:path*", // Redirect all "/smartflow" requests
+        destination: "https://smartflow-sigma.vercel.app/:path*", // Your SmartFlow project URL
+      },
+    ];
+  },
 };
 
 export default nextConfig;
